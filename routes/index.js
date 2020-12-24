884b743d106612818df6b6e5aa076ce37207d71c
medbenchaaben <med.benchaaben@gmail.com>
2020-12-24 22:03:40 +0100

    maj for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var user = require('./user');
+router.get("/", function(req, res) {
+    console.log("here");
+ 
+ });
 router.use('/user', user);
 module.exports = router;
